9888b90395f2b72cc72167784863cab82480bff6
Timo <38291523+lovetodream@users.noreply.github.com>
2022-12-18 18:19:23 +0100

    chore(deps): use custom sql-kit version temporarily

--- Package.swift
+++ Package.swift
@@ -16,19 +16,18 @@ let package = Package(
     ],
     dependencies: [
          .package(url: "https://github.com/lovetodream/oracle-nio.git", branch: "main"),
-         .package(url: "https://github.com/vapor/sql-kit.git", from: "3.23.0"),
+         .package(url: "https://github.com/lovetodream/sql-kit.git", branch: "main"), // use vapor version once PR#
          .package(url: "https://github.com/vapor/async-kit.git", from: "1.14.0"),
     ],
     targets: [
-        .target(
-            name: "OracleKit",
-            dependencies: [
-                .product(name: "OracleNIO", package: "oracle-nio"),
-                .product(name: "AsyncKit", package: "async-kit"),
-                .product(name: "SQLKit", package: "sql-kit"),
-            ]),
-        .testTarget(
-            name: "OracleKitTests",
-            dependencies: ["OracleKit"]),
+        .target(name: "OracleKit", dependencies: [
+            .product(name: "OracleNIO", package: "oracle-nio"),
+            .product(name: "AsyncKit", package: "async-kit"),
+            .product(name: "SQLKit", package: "sql-kit"),
+        ]),
+        .testTarget(name: "OracleKitTests", dependencies: [
+            .product(name: "SQLKitBenchmark", package: "sql-kit"),
+            .target(name: "OracleKit"),
+        ]),
     ]
 )
