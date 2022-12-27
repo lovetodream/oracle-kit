422926abc16df398aea3efa7fff506b5afed60e1
Timo <38291523+lovetodream@users.noreply.github.com>
2022-12-27 13:56:09 +0100

    Revert "feat: support upsert syntax"
    This reverts commit d07e41bb559db0c555946ee19a1af3290e456e59.

--- Sources/OracleKit/OracleDialect.swift
+++ Sources/OracleKit/OracleDialect.swift
@@ -25,7 +25,7 @@ public struct OracleDialect: SQLDialect {
         ])
     }
     public var alterTableSyntax: SQLAlterTableSyntax { .init(alterColumnDefinitionClause: SQLRaw("MODIFY"), allowsBatch: true) }
-    public var upsertSyntax: SQLUpsertSyntax { .standard }
+    public var upsertSyntax: SQLUpsertSyntax { .unsupported }
     public var supportsReturning: Bool { true }
     public var supportsIfExists: Bool { false }
     public var unionFeatures: SQLUnionFeatures { [.union, .unionAll, .intersect, .explicitDistinct, .parenthesizedSubqueries] }
